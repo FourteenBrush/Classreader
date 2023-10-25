53178e6bb7423ee7e23d629cad5b287e0217eb88
FourteenBrush <naessensarthur2@protonmail.com>
2023-10-25 20:31:01 +0200

    Add some more java code

--- res/Test.java
+++ res/Test.java
@@ -1,9 +1,34 @@
 package res;
+
 public class Test {
     @SuppressWarnings("unused")
     public static void main(String[] args) {
         String s1 = "some string";
         String s2 = "some other string";
         String s3 = s1 + s2;
+
+        Runnable r = () -> System.out.println(new InnerClass());
+    }
+
+
+    static class InnerClass {
+        static final int MASK = 0x2;
+
+        @SuppressWarnings("unused")
+        private transient float a;
+    }
+
+    public interface SomeInterface {
+        void foo();
+    }
+
+    class SomeClass implements SomeInterface {
+        @Override
+        public void foo() {
+            System.out.println(InnerClass.MASK);
+        }
+
+        @Deprecated
+        native void i_ret();
     }
 }
